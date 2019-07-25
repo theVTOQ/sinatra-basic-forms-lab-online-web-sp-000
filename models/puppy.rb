0bc50e90623267afedb066cb908dc3c8acd8dccc
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-07-25 05:40:48 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,6 +1,6 @@
 class Puppy
   attr_accessor :name, :breed, :months_old
-  def initialize(name: :name, breed: :breed, months_old: :months_old)
+  def initialize(name: name, breed: breed, months_old: months_old)
     @name = name
     @breed = breed
     @months_old = months_old
